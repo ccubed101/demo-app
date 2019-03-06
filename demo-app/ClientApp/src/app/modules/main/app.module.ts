a7c27e90e589fb45ed53a8955b70cce8c627550a
ccubed101 <ccubed101@gmail.com>
2019-03-06 18:40:28 -0500

    Added epanding navigation menu. (2)

--- demo-app/ClientApp/src/app/modules/main/app.module.ts
+++ demo-app/ClientApp/src/app/modules/main/app.module.ts
@@ -12,37 +12,41 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/app/nav-menu/nav-menu.component';
 import { SplashComponent } from './components/app/splash/splash.component';
+import { ExpandingNavMenuComponent } from './components/app/expanding-nav-menu/expanding-nav-menu.component';
+import { NavMenuItemComponent } from './components/app/expanding-nav-menu/nav-menu-item.component';
 import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavMenuComponent,
-    SplashComponent
+		AppComponent,
+		NavMenuComponent,
+		SplashComponent,
+		ExpandingNavMenuComponent,
+		NavMenuItemComponent,
   ],
   imports: [
-    // Add .withServerTransition() to support Universal rendering.  Note that 'app-root'
-    // identifies the element in index.html where the Angular generated html is placed.
-    BrowserModule.withServerTransition({ appId: 'app-root' }),
-    HttpClientModule,
-    FormsModule,
-    AppRoutingModule,
-    CoreModule,                     // For application wide (global) services.  This will add providers from CoreModule to AppModule's providers.
-    SharedModule.forRoot(),         // For stuff that is shared across multiple apps.  This will add providers from SharedModule to AppModule's providers. 
+		// Add .withServerTransition() to support Universal rendering.  Note that 'app-root'
+		// identifies the element in index.html where the Angular generated html is placed.
+		BrowserModule.withServerTransition({ appId: 'app-root' }),
+		HttpClientModule,
+		FormsModule,
+		AppRoutingModule,
+		CoreModule,                     // For application wide (global) services.  This will add providers from CoreModule to AppModule's providers.
+		SharedModule.forRoot(),         // For stuff that is shared across multiple apps.  This will add providers from SharedModule to AppModule's providers. 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 
-  // Next 7 lines added to support Angular Universal (server-side rendering).
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object,
-    @Inject(APP_ID) private appId: string) {
-    const platform = isPlatformBrowser(platformId) ?
-      'in the browser' : 'on the server';
-    console.log(`Running ${platform} with appId=${appId}`);
-  }
+	  // Next 7 lines added to support Angular Universal (server-side rendering).
+	  constructor(
+			@Inject(PLATFORM_ID) private platformId: Object,
+			@Inject(APP_ID) private appId: string) {
+			const platform = isPlatformBrowser(platformId) ?
+			  'in the browser' : 'on the server';
+			console.log(`Running ${platform} with appId=${appId}`);
+	  }
 
 }
