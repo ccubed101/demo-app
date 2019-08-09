ea7173aef32d161bd15997c6e577cef4bb398905
ccubed101 <ccubed101@gmail.com>
2019-08-09 15:30:31 -0400

    Configuring Kestral to use HTTPS.

--- demo-app/Program.cs
+++ demo-app/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Security.Cryptography.X509Certificates;
 
 namespace demo_app
 {
@@ -14,7 +15,29 @@ namespace demo_app
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+			// Creating an instance of a ConfigurationBuilder looks to be the only way 
+			// gain access to configuration data in the this (i.e the Main) method.
+			var config = new ConfigurationBuilder()
+			   .SetBasePath(Directory.GetCurrentDirectory())
+			   .AddJsonFile("appsettings.json", optional: true)
+			   .Build();
+
+			CreateWebHostBuilder(args)
+
+				// The following section configures Kestrel to use HTTPS.
+				// Not sure what effect the following statements have if the application is served by IIS (via Docker).
+				.UseKestrel((options) => {
+					options.ConfigureHttpsDefaults((co) =>
+					{
+						// Certificate used to encrypt data over TLS (i.e. SSL)
+						co.ServerCertificate = new X509Certificate2(config.GetValue<string>("NameOfX509CertificateFileUsedForKestrelHTTPS"));
+					});
+				})
+				// Specifies where Kestrel listens for encrypted requests.
+				.UseUrls(config.GetValue<string>("IPAddressThatKestrelListensForRequests"))
+
+				.Build()
+				.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
