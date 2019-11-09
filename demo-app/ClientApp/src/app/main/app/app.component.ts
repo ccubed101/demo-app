2c9e819699b4c0ff4785f1fec810dde5611ff9e1
ccubed101 <ccubed101@gmail.com>
2019-11-09 10:03:09 -0500

    Added configuration file for Main module.  Configuration data will be centralized there.

--- demo-app/ClientApp/src/app/main/app/app.component.ts
+++ demo-app/ClientApp/src/app/main/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavMenuItem } from '../app/expanding-nav-menu/NavMenuItem';
 
+import { configuration } from '../configuration';
+
 @Component({
     selector: 'app-root',
 	template: `
@@ -12,44 +14,8 @@ import { NavMenuItem } from '../app/expanding-nav-menu/NavMenuItem';
   ]
 })
 export class AppComponent {
-	title = 'app';
 
-	navMenuTree: NavMenuItem[] = [
-		new NavMenuItem("Splash", "/", null),
-		new NavMenuItem("JWT Demo", "JwtDemo", null),
-		new NavMenuItem("Commit History", "analytics", null),
-		new NavMenuItem("Single Video", "Videos/SingleVideo", null),
-		new NavMenuItem("Animations", null, [
-			new NavMenuItem("Route Transitions", null, [
-				new NavMenuItem("Route Transition Page #1", "Animations/AnimationsRoot/RouteTransitionPage1", null),
-				new NavMenuItem("Route Transition Page #2", "Animations/AnimationsRoot/RouteTransitionPage2", null),
-			]),
-			new NavMenuItem("Animated List", "Animations/AnimatedList", null),
-			new NavMenuItem("Rotation", "Animations/Rotation", null),
-			new NavMenuItem("Rotator Wrapper", "Animations/RotatorWrapper", null),
-		]),
-		new NavMenuItem("Routing", "RoutingExamples", [
-			new NavMenuItem("Routing Example", "RoutingExamples", null),
-			new NavMenuItem("Route Parameter Examples", "RoutingExamples/RouteParameterExamples", null),
-		]),
-		new NavMenuItem("Reactive Forms Examples", null, [
-			new NavMenuItem("Reactive Forms Example #1", "reactive-forms-demo/reactive-forms-example1", null),
-			new NavMenuItem("Reactive Forms Example #2", "reactive-forms-demo/reactive-forms-example2", null),
-			new NavMenuItem("Reactive Forms Example #3", "reactive-forms-demo/reactive-forms-example3", null),
-		]),
-	];
+    title = 'app';
 
-	//navMenuTree: { Text: string, Route: string, Children: {} }[] = [
-	//	{ Text: "Splash", Route: "/", Children: null },
-	//	{ Text: "Commit History", Route: "reactive-forms-demo", Children: null },
-	//	{ Text: "Reactive Forms Examples", Route: null, Children: [
-	//			{ Text: "Reactive Forms Example #1", Route: "reactive-forms-demo/reactive-forms-example1", Children: null },
-	//			{ Text: "Reactive Forms Example #2", Route: "reactive-forms-demo/reactive-forms-example2", Children: null },
-	//			{ Text: "Reactive Forms Example #3", Route: "reactive-forms-demo/reactive-forms-example3", Children: null },
-	//		]
-	//	},
-	//];
-
-
-
+    navMenuTree: NavMenuItem[] = configuration.navMenuTree;
 }
