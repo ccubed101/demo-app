4ac520b5fe179d3437a5711178b1b08a0ef404c6
ccubed101 <ccubed101@gmail.com>
2019-11-11 11:14:35 -0500

    Removing .js files that do not need to be in repository (because that are transpiled from .ts files).

--- demo-app/ClientApp/src/app/BreezeDemo/Configuration.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configuration = {
-    UrlForBreezeBackEndService: "https://localhost:443/breeze/SchoolModel",
-};
-//# sourceMappingURL=Configuration.js.map
