ee246cc4ef6111f2fdaa43d7d5e24e855b250b6c
ccubed101 <ccubed101@gmail.com>
2019-12-01 12:41:16 -0500

    Commenting out some code related to running the app inside Visual Studio that is now marked as obsolete and is going to be removed in the future.

--- demo-app/Startup.cs
+++ demo-app/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+//using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.AspNetCore.SpaServices.Extensions;		// Added as part of migration to ASP.NET Core 3.0.  It replaces Microsoft.AspNetCore.SpaServices which is now obsolete.
 
 namespace demo_app
 {
@@ -73,46 +75,58 @@ namespace demo_app
                     template: "{controller}/{action=Index}/{id?}");
             });
 
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
+			app.UseSpa(spa =>
+			{
+				spa.Options.SourcePath = "ClientApp";
 
-                spa.Options.SourcePath = "ClientApp";
 
-				// **** Added to support Server Side Rendering ****
-				spa.UseSpaPrerendering(options =>
-				{
-					// The path specified in the statement below should probably be different
-					// from the path specified above, configuration.RootPath = "ClientApp/dist";
-					// The reason is that the "outputPath" property of the "build" target in 
-					// the angular.json file will specify "dist" for sake of convenience.  While
-					// it may seem nice to have the client code and server-side rendering code in 
-					// the same directory problems could arise during a VS Publish operation if 
-					// the contents of the output directory (i.e. "dist") is deleted at the start
-					// of the operation becasue the directory already contain the server-side
-					// rendering code.
-					// 
-					options.BootModulePath = $"{spa.Options.SourcePath}/dist-server/main.js";
-					options.BootModuleBuilder = env.IsDevelopment()
-						? new AngularCliBuilder(npmScript: "build:ssr")
-						: null;
-					options.ExcludeUrls = new[] { "/sockjs-node" };
-				});
-				// **** Added to support Server Side Rendering ****
+				// The sections below were commented out when the migration to ASP.NET Core 3.0
+				// occurred.  It appears that it is no longer necessary. It only really mattered 
+				// if you wanted to run the app from within Visual Studio using the green triangle 
+				// that is used to launch "demo-app".  You can still do the same (without the code
+				// below) but instead of using the port specified in the Properties pages (which 
+				// is no longer there) you simply use https://localhost.  Not sure how server 
+				// side rendering is affected.  It may still be necessary to manually execute 
+				// "ng build:ssr".
 
-				if (env.IsDevelopment())
-                {
-					// From https://docs.microsoft.com/en-us/aspnet/core/client-side/spa/angular?view=aspnetcore-2.1&tabs=netcore-cli#server-side-rendering
-					// Around 10 seconds is required to start the Angular CLI server when you try and run your app from within VS.  
-					// If you want to avoid this delay then comment out the line below and comment in the line below that.  Then 
-					// manually start the Angular CLI server from a command prompt within the "ClientApp" directory using "npm start".
-					// Then when you run your app from within VS you can simply leave the browser running (no need to stop it) and
-					// then modify the client app's file as desired and saving the modifications when you want to see them rendered.
-					//spa.UseAngularCliServer(npmScript: "start");
-					spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
-				}
+				//// To learn more about options for serving an Angular SPA from ASP.NET Core,
+				//// see https://go.microsoft.com/fwlink/?linkid=864501
+
+				//// **** Added to support Server Side Rendering ****
+				//spa.UseSpaPrerendering(options =>
+				//{
+				//	// The path specified in the statement below should probably be different
+				//	// from the path specified above, configuration.RootPath = "ClientApp/dist";
+				//	// The reason is that the "outputPath" property of the "build" target in 
+				//	// the angular.json file will specify "dist" for sake of convenience.  While
+				//	// it may seem nice to have the client code and server-side rendering code in 
+				//	// the same directory problems could arise during a VS Publish operation if 
+				//	// the contents of the output directory (i.e. "dist") is deleted at the start
+				//	// of the operation becasue the directory already contain the server-side
+				//	// rendering code.
+				//	// 
+				//	options.BootModulePath = $"{spa.Options.SourcePath}/dist-server/main.js";
+				//	options.BootModuleBuilder = env.IsDevelopment()
+				//		? new AngularCliBuilder(npmScript: "build:ssr")
+				//		: null;
+				//	options.ExcludeUrls = new[] { "/sockjs-node" };
+				//});
+				//// **** Added to support Server Side Rendering ****
+
+				//if (env.IsDevelopment())
+				//{
+				//	// From https://docs.microsoft.com/en-us/aspnet/core/client-side/spa/angular?view=aspnetcore-2.1&tabs=netcore-cli#server-side-rendering
+				//	// Around 10 seconds is required to start the Angular CLI server when you try and run your app from within VS.  
+				//	// If you want to avoid this delay then comment out the line below and comment in the line below that.  Then 
+				//	// manually start the Angular CLI server from a command prompt within the "ClientApp" directory using "npm start".
+				//	// Then when you run your app from within VS you can simply leave the browser running (no need to stop it) and
+				//	// then modify the client app's file as desired and saving the modifications when you want to see them rendered.
+				//	spa.UseAngularCliServer(npmScript: "start");
+				//	//spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+				//}
+
+
 			});
-        }
+		}
     }
 }
