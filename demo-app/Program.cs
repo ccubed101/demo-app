9e9e147b32674df9a290381b0e2e04e6734ddf3f
ccubed101 <ccubed101@gmail.com>
2019-12-03 13:32:59 -0500

    Just added some comments to Program.cs.

--- demo-app/Program.cs
+++ demo-app/Program.cs
@@ -22,7 +22,7 @@ namespace demo_app
 			   .AddJsonFile("appsettings.json", optional: true)
 			   .Build();
 
-			CreateWebHostBuilder(args)
+			CreateWebHostBuilder(args)	
 
 				// The following section configures Kestrel to use HTTPS.
 				// Not sure what effect the following statements have if the application is served by IIS (via Docker).
@@ -33,9 +33,18 @@ namespace demo_app
 						co.ServerCertificate = new X509Certificate2(config.GetValue<string>("NameOfX509CertificateFileUsedForKestrelHTTPS"));
 					});
 				})
-				// If you want to host app using IIS and the "In-process" model the call UseIIS().
+				// If you want to use IIS and the "In-process" model to host this app then call UseIIS().
+				// Calling this method makes it possible to run the app in development mode using the 
+				// green triangle in the VS toolbar when "IIS Express" is selected.  Also manually setting
+				// up to use IIS on this development machine to host the app only works if this method is 
+				// called.
 				.UseIIS()
 				// If you want to host app using IIS and the "Out-of-process" model the call UseIISIntegration(). 
+				// The default model is "In-process".  If you want to use "Out-of-process" then add the following
+				// to the .csproj file,
+				//   <PropertyGroup>
+				//	     <AspNetCoreHostingModel>OutOfProcess</AspNetCoreHostingModel>
+				//   </PropertyGroup>
 				.UseIISIntegration()
 				// Specifies where Kestrel listens for encrypted requests.
 				.UseUrls(config.GetValue<string>("IPAddressThatKestrelListensForRequests"))
