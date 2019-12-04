623307a654f2f2525592bf08b210252ceb46bffa
ccubed101 <ccubed101@gmail.com>
2019-12-04 16:44:36 -0500

    Add a URL for BreezeDataService that is used when service is launched from demo-app docker-compose file.

--- demo-app/ClientApp/src/app/BreezeDemo/Configuration.ts
+++ demo-app/ClientApp/src/app/BreezeDemo/Configuration.ts
@@ -1,3 +1,8 @@
 export let configuration = {
-    UrlForBreezeBackEndService: "https://localhost:443/breeze/SchoolModel",
+
+    // Use this URL when running BreezeDataService from Visual Studio.
+    //UrlForBreezeBackEndService: "https://localhost:443/breeze/SchoolModel",
+
+    // Use this URL when launching BreezeDataService via demo-app docker-compose file.
+    UrlForBreezeBackEndService: "https://localhost:9008/breeze/SchoolModel",
 }
