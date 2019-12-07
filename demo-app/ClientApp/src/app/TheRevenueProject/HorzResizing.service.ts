6a57624673d2b6c354ab134cbf44f63c57183d66
ccubed101 <ccubed101@gmail.com>
2019-12-07 09:15:54 -0500

    Added services for horizontal and vertical resizing of panels (DIVs) and code to demonstrate the functionality in TheRevenueProjectRoot component.

---
+++ demo-app/ClientApp/src/app/TheRevenueProject/HorzResizing.service.ts
@@ -0,0 +1,141 @@
+import * as $ from 'jquery'
+import 'jqueryui'
+
+export interface IHorzResizingService {
+    Setup(selectors: string[], stopCallback: () => void): void;
+    Enable(): void;
+    Disable(): void;
+}
+
+
+class Panel {
+    div: JQuery<HTMLDivElement>;
+    initialWidth: number;
+}
+
+export class HorzResizingService implements IHorzResizingService {
+
+    // Construction.
+
+    constructor() { }
+
+
+    // Instance variables.
+
+    panels: Panel[] = [];
+
+    stopCallback: () => void = null;
+
+
+    // Public methods.
+
+    // Setup for resizing operation.  Must be called before a resize operation can be performed.
+    public Setup(selectors: string[], stopCallback: () => void): void {
+
+        this.stopCallback = stopCallback;
+
+        selectors.forEach((selector: string) => {
+            let panel: Panel = new Panel();
+            panel.div = $(selector);            // This statement cannot be executed in the constructor because the DOM does not exist at that point.
+            this.panels.push(panel);
+        })
+
+        this.panels.forEach((panel: Panel, index: number, panels: Panel[]) => {
+            // If there is only one panel.
+            if (index === 1 && panels.length === 1) {
+                panel.div.resizable({ handles: "e", start: this.Start.bind(this), resize: this.Resize.bind(this), stop: this.Stop.bind(this) });
+            }
+            // If there are 2 or more panels.
+            else if (index < panels.length - 1) {
+                panel.div.resizable({ handles: "e", start: this.Start.bind(this), resize: this.Resize.bind(this), stop: this.Stop.bind(this) });
+                panel.div.resizable("option", "maxWidth", panels[index].initialWidth + panels[index + 1].initialWidth - 50);
+                panel.div.resizable("option", "minWidth", 50);
+            }
+        })
+    }
+
+    // Enable resizing service.
+    public Enable(): void {
+        this.panels.forEach((panel: Panel) => {
+            panel.div.resizable("enable");
+        })
+    }
+
+    // Disable resizing service.
+    public Disable(): void {
+        this.panels.forEach((panel: Panel) => {
+            $(panel.div).resizable("disable");
+        })
+    }
+
+
+    // Event handlers.
+
+    // This event is triggered at the start of a resize operation.
+    // Params:
+    //      e           Normalized event object which will contain properties that will always be present regardless of which browser is being used.
+    //      ui          Object which has the following properties: element, helper, originalElement, originalPosition, originalSize, position and size.
+    Start(e: any, ui: any): void {
+
+        /* Record the widths of the panels. */
+        this.panels.forEach((panel: Panel) => {
+            var widthString = panel.div.css("width");
+            panel.initialWidth = Number(widthString.substring(0, widthString.length - 2));
+        })
+
+        this.panels.forEach((panel: Panel, index: number, panels: Panel[]) => {
+            // If there is only one panel.
+            if (index === 1 && panels.length === 1) {
+            }
+            // If there are 2 or more panels.
+            else if (index < panels.length - 1) {
+                panel.div.resizable("option", "maxWidth", panels[index].initialWidth + panels[index + 1].initialWidth - 50);
+                panel.div.resizable("option", "minWidth", 50);
+            }
+        })
+    }
+
+    // This event is triggered during the resize, on the drag of the resize handler.
+    // Params:
+    //      e           Normalized event object which will contain properties that will always be present regardless of which browser is being used.
+    //      ui          Object which has the following properties: element, helper, originalElement, originalPosition, originalSize, position and size.
+    Resize(e: any, ui: any): void {
+
+        this.panels.forEach((panel: Panel, index: number, panels: Panel[]) => {
+            if (ui.element[0].id === panel.div.attr('id')) {
+                if (ui.size.width < 0) {
+                    $(panels[index + 1].div).css("width", panels[index].initialWidth + panels[index + 1].initialWidth);
+                }
+                if (ui.size.width > panels[index].initialWidth + panels[index + 1].initialWidth) {
+                    $(panels[index + 1].div).css("width", 0);
+                }
+                else {
+                    $(panels[index + 1].div).css("width", panels[index + 1].initialWidth + (panels[index].initialWidth - ui.size.width));
+                }
+            }
+        })
+    }
+
+    // This event is triggered at the end of a resize operation.
+    // When the resize operation is completed convert the panel size from pixels back to percentages.
+    // Doing so means that when the browser is resized the panels will keep their relative sizes.
+    // Params:
+    //      e           Normalized event object which will contain properties that will always be present regardless of which browser is being used.
+    //      ui          Object which has the following properties: element, helper, originalElement, originalPosition, originalSize, position and size.
+    Stop(e: any, ui: any): void {
+
+        var total = 0;
+        this.panels.forEach((panel: Panel, index: number, panels: Panel[]) => {
+            panel.initialWidth = panel.div.outerWidth();
+            total += panel.initialWidth;
+        })
+
+        this.panels.forEach((panel: Panel, index: number, panels: Panel[]) => {
+            let widthString: string = ((panel.initialWidth / total) * 100.0).toString() + "%";
+            panel.div.css("width", widthString);
+        })
+
+        if (this.stopCallback)
+            this.stopCallback();
+    }
+}
--- demo-app/ClientApp/src/app/TheRevenueProject/TheRevenueProjectRootComponent/TheRevenueProjectRoot.component.ts
+++ demo-app/ClientApp/src/app/TheRevenueProject/TheRevenueProjectRootComponent/TheRevenueProjectRoot.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterState, RouterOutlet } from '@angular/router';
+import { HorzResizingService } from '../HorzResizing.service'
+import { VertResizingService } from '../VertResizing.service'
+
+import * as $ from 'jquery'
+import 'jqueryui'
+
+//import { AppStatePersistenceService as ASPS } from '../Services/AppStatePersistence.Service'
+//import { IAppModel, AppModel } from '../Models/AppModel'
 
 @Component({
     selector: 'the-revenue-project',
@@ -7,18 +15,72 @@ import { ActivatedRoute, RouterState, RouterOutlet } from '@angular/router';
 		<div class="title">
 			The Revenue Project
         </div>
+        <div>
+            <div id="leftPanel" class="panel" style="background-color: red; width: 33.33%; height: 800px">
+            </div>
+            <div id="centerPanel" class="panel" style="background-color: green; width: 33.33%; height: 800px">
+                <div id="topPanel" style="background-color: green; height: 33.33%">
+                </div>
+                <div id="middlePanel" style="background-color: cyan; height: 33.33%">
+                </div>
+                <div id="bottomPanel" style="background-color: yellow; height: 33.33%">
+                </div>
+            </div>
+            <div id="rightPanel" class="panel" style="background-color: blue; width: 33.33%; height: 800px">
+            </div>
+        </div>
     `,
 	styles: [
-		".title { text-align: center; font-size: xx-large; margin-top: 0.5em }",
-	],
+        ".title { text-align: center; font-size: xx-large; margin-top: 0.5em }",
+        ".panel { height: 100%; float: left; overflow: hidden; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;}"
+    ],
+    providers: [
+        HorzResizingService,
+        VertResizingService,
+    ]
 })
-export class TheRevenueProjectRootComponent {
+export class TheRevenueProjectRootComponent implements OnInit {
 
     // Construction.
 
-    constructor(
-    ) {
+    constructor() { }
+
+
+    // Instance variables.
+    private horzResizingService: HorzResizingService = new HorzResizingService();
+    private vertResizingService: VertResizingService = new VertResizingService();
+
+
+    // Life-cycle
+
+    ngOnInit() {
+        this.horzResizingService.Setup(["#leftPanel", "#centerPanel", "#rightPanel"], this.AfterHorzResizing.bind(this));
+        this.vertResizingService.Setup(["#topPanel", "#middlePanel", "#bottomPanel"], this.AfterVertResizing.bind(this));
     }
+
+
+    // Methods.
+
+    AfterHorzResizing() {
+
+        //// Persist records of widths so that they can be restored the next time
+        //// the application is executed.
+        //ASPS.Persist(ASPS.LeftPanelWidth, leftPanelWidthString);
+        //ASPS.Persist(ASPS.CenterPanelWidth, centerPanelWidthString);
+        //ASPS.Persist(ASPS.RightPanelWidth, rightPanelWidthString);
+
+        //// Keep a record of the widths so that they can be restored when the 
+        //// user navigates away from a "Print Preview" tab.
+        //this.AppModel.LeftPanelWidth = leftPanelWidthString;
+        //this.AppModel.CenterPanelWidth = centerPanelWidthString;
+        //this.AppModel.RightPanelWidth = rightPanelWidthString;
+
+    }
+
+    AfterVertResizing() {
+
+    }
+
 }
 
 
