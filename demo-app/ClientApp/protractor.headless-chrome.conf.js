106538f609c26db0bcb49c88cf8fab7b24fdec32
ccubed101 <ccubed101@gmail.com>
2020-02-21 12:20:23 -0500

    Just some minor reformatting.  No changes.

--- demo-app/ClientApp/protractor.headless-chrome.conf.js
+++ demo-app/ClientApp/protractor.headless-chrome.conf.js
@@ -4,28 +4,29 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
-  ],
+    allScriptsTimeout: 11000,
+    specs: [
+        './e2e/**/*.e2e-spec.ts'
+    ],
 	capabilities: {
-	  chromeOptions: {
-		args: [ "--headless" ]
-	  },
-	  'browserName': 'chrome'
+        chromeOptions: {
+            args: [ "--headless" ]
+        },
+        'browserName': 'chrome'
 	},
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+    directConnect: true,
+    baseUrl: 'http://localhost:4200/',
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        print: function() {}
+    },
+    onPrepare() {
+        require('ts-node').register({
+            project: 'e2e/tsconfig.e2e.json'
+        });
+        jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    },
+    Chrome
 };
