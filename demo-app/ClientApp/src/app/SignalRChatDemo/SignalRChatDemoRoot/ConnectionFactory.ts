c8a4329f2c107553eed25bd30c42f6540c7dd819
ccubed101 <ccubed101@gmail.com>
2020-02-21 12:24:34 -0500

    Make ConnectionFactory selection of URLs environment dependent.

--- demo-app/ClientApp/src/app/SignalRChatDemo/SignalRChatDemoRoot/ConnectionFactory.ts
+++ demo-app/ClientApp/src/app/SignalRChatDemo/SignalRChatDemoRoot/ConnectionFactory.ts
@@ -1,36 +1,42 @@
 import * as SignalR from "@microsoft/signalr";
 
+import { environment } from './../../../environments/environment';
+
 export class ConnectionFactory {
 
     // Construction.
 
     constructor() {
 
-        //this.connections.push(
-        //    new SignalR.HubConnectionBuilder()
-        //        .withUrl("http://localhost:9010/ChatHub")
-        //        .configureLogging(SignalR.LogLevel.Information)
-        //        .build()
-        //);
-        //this.connections.push(
-        //    new SignalR.HubConnectionBuilder()
-        //        .withUrl("http://localhost:9010/ChatHub")
-        //        .configureLogging(SignalR.LogLevel.Information)
-        //        .build()
-        //);
+        if (environment.production == true) {
+            this.connections.push(
+                new SignalR.HubConnectionBuilder()
+                    .withUrl("http://localhost:9010/ChatHub")
+                    .configureLogging(SignalR.LogLevel.Information)
+                    .build()
+            );
+            this.connections.push(
+                new SignalR.HubConnectionBuilder()
+                    .withUrl("http://localhost:9010/ChatHub")
+                    .configureLogging(SignalR.LogLevel.Information)
+                    .build()
+            );
+        }
 
-        this.connections.push(
-            new SignalR.HubConnectionBuilder()
-                .withUrl("https://localhost:44362/ChatHub")
-                .configureLogging(SignalR.LogLevel.Information)
-                .build()
-        );
-        this.connections.push(
-            new SignalR.HubConnectionBuilder()
-                .withUrl("https://localhost:44362/ChatHub")
-                .configureLogging(SignalR.LogLevel.Information)
-                .build()
-        );
+        else if (environment.production == false) {
+            this.connections.push(
+                new SignalR.HubConnectionBuilder()
+                    .withUrl("https://localhost:44362/ChatHub")
+                    .configureLogging(SignalR.LogLevel.Information)
+                    .build()
+            );
+            this.connections.push(
+                new SignalR.HubConnectionBuilder()
+                    .withUrl("https://localhost:44362/ChatHub")
+                    .configureLogging(SignalR.LogLevel.Information)
+                    .build()
+            );
+        }
    }
 
 
